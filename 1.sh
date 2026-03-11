03f1f23d7e1bd15741f8e91d7ae24a0f122b41a6
HunorDomokos <hunihuni250@gmail.com>
2026-03-11 20:43:39 +0200

    First commit

---
+++ 1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [[ ! -w . ]]; then
+	echo "Keine Schreibrechte in $(pwd)"
+	exit
+fi
+
+if [[ ! -x . ]]; then
+	echo "Keine Ausfuhrungsrechte in $(pwd)"
+	exit
+fi
+
+if [[ $# != 2 ]]; then
+	echo "Es mussen 2 Argumente ubergeben werden"
+	exit
+fi
+
+if [[ ! $1 =~ ^[0-9]+$ ]]; then
+	echo "Anzahl der Ordner muss eine ganze Zahl sein"
+	exit
+fi
+
+if [[ ! $2 =~ ^[0-9]+$ ]]; then
+	echo "Anzahl der Dateien muss eine ganze Zahl sein"
+	exit
+fi
+
+for (( i=1 ; i <= $1 ; i++ )); do
+	ordner_name="ordner$i"
+
+	if [[ -e "$ordner_name" ]]; then
+		echo "ordner$i existiert bereits -> wird ubersprungen"
+		continue
+	fi
+
+	if  ! mkdir "$ordner_name" 2>/dev/null; then
+		echo "$ordner_name konnte nich erstellt werden"
+		exit
+	fi
+
+
+for (( j=1 ; j <= $2 ; j++ )); do
+	touch "$ordner_name/datei$j.txt"
+done
+done
+
